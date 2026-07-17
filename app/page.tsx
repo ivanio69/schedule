5b93054ff5243090755f22ee0142a6aca4ce9fd4
Ivan Mikhneev <ivanio@Ivans-MacBook-Air.local>
2026-07-17 04:43:37 +0300

    dev0.1

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,195 @@
-import Image from "next/image";
+"use client";
+import {
+  ToggleButtonGroup,
+  ToggleButton,
+  Card,
+  Chip,
+  Tabs,
+  Pagination,
+} from "@heroui/react";
+import { useSwipeable } from "react-swipeable";
+import { useState, useEffect } from "react";
+import { motion, AnimatePresence, MotionConfig } from "motion/react";
+
+import table from "./table.json";
 
 export default function Home() {
+  let lsGroup = false;
+  if (typeof window !== "undefined") {
+    lsGroup = localStorage.getItem("group") == "true";
+  }
+  const groupC = () => lsGroup;
+  function weeksBetween(d1, d2) {
+    return Math.round((d2 - d1) / (7 * 24 * 60 * 60 * 1000));
+  }
+  const [group, setGroup] = useState(groupC);
+  const [day, setDay] = useState(0);
+  const [page, setPage] = useState(
+    Math.abs(
+      weeksBetween(
+        new Date(),
+        new Date(
+          table.semesterStart[0],
+          table.semesterStart[1],
+          table.semesterStart[2],
+        ),
+      ),
+    ),
+  );
+
+  const handlers = useSwipeable({
+    onSwiped: (eventData) => {
+      if (eventData.dir == "Right" && page >= 1) {
+        setPage(page - 1);
+      } else if (eventData.dir == "Left" && page <= totalPages) {
+        setPage(page + 1);
+      } else;
+    },
+    preventScrollOnSwipe: true,
+  });
+
+  const totalPages = 18;
+
+  const getPageNumbers = () => {
+    const pages: (number | "ellipsis")[] = [];
+
+    pages.push(1);
+
+    if (page > 3) {
+      pages.push("ellipsis");
+    }
+
+    const start = Math.max(2, page - 1);
+    const end = Math.min(totalPages - 1, page + 1);
+
+    for (let i = start; i <= end; i++) {
+      pages.push(i);
+    }
+
+    if (page < totalPages - 2) {
+      pages.push("ellipsis");
+    }
+
+    pages.push(totalPages);
+
+    return pages;
+  };
+
+  useEffect(() => {
+    localStorage.setItem("group", group ? "true" : "false");
+  }, [group]);
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div>
+      <div className="flex flex-wrap max-w-200 p-5 justify-between items-center align-middle content-center gap-y-5">
+        <div className="flex justify-between items-center">
+          <p className="mx-7">Подгруппы</p>
+          <ToggleButtonGroup selectionMode="multiple">
+            <ToggleButton isSelected={!group} onChange={(v) => setGroup(!v)}>
+              1
+            </ToggleButton>
+            <ToggleButton isSelected={group} onChange={setGroup}>
+              <ToggleButtonGroup.Separator />2
+            </ToggleButton>
+          </ToggleButtonGroup>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <Tabs className="w-full max-w-md justify-center">
+          <Tabs.ListContainer>
+            <Tabs.List aria-label="Options">
+              <Tabs.Tab id="0" onClick={() => setDay(0)}>
+                ПН
+                <Tabs.Indicator />
+              </Tabs.Tab>
+              <Tabs.Tab id="1" onClick={() => setDay(1)}>
+                ВТ
+                <Tabs.Indicator />
+              </Tabs.Tab>
+              <Tabs.Tab id="2" onClick={() => setDay(2)}>
+                СР
+                <Tabs.Indicator />
+              </Tabs.Tab>
+              <Tabs.Tab id="3" onClick={() => setDay(3)}>
+                ЧТ
+                <Tabs.Indicator />
+              </Tabs.Tab>
+              <Tabs.Tab id="4" onClick={() => setDay(4)}>
+                ПТ
+                <Tabs.Indicator />
+              </Tabs.Tab>
+              <Tabs.Tab id="5" onClick={() => setDay(5)}>
+                СБ
+                <Tabs.Indicator />
+              </Tabs.Tab>
+            </Tabs.List>
+          </Tabs.ListContainer>
+        </Tabs>
+      </div>
+      <div className="max-w-200 p-5" {...handlers}>
+        <AnimatePresence initial={false}>
+          {table.days[day].table.map((e) => {
+            if (
+              ((e.group.length == 1 && (e.group[0] == 1) !== group) ||
+                (e.group.length == 1 && (e.group[0] == 2) == group) ||
+                e.group.length == 2) &&
+              e.weeks.includes(page)
+            )
+              return (
+                <motion.div
+                  initial={{ opacity: 0, x: -100 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  exit={{ opacity: 0, x: 100 }}
+                  transition={{ duration: 0.3, ease: "easeInOut" }}
+                  key={e.timeStart + e.class}
+                >
+                  <Card className="my-5">
+                    <Card.Content>
+                      <div className="flex justify-between items-center px-5">
+                        <div>
+                          <p className="text-xl font-bold">{e.class}</p>
+                          {e.group.length !== 2 && (
+                            <Chip variant="secondary" className="my-2">
+                              {e.group[0]} подгруппа
+                            </Chip>
+                          )}
+                          <p className="">{e.professor}</p>
+                          <p className="text-muted">{e.auditorium}</p>
+                        </div>
+                        <div>
+                          <p className="text-l px-4 py-2 bg-accent rounded-full">
+                            {e.timeStart}
+                          </p>
+                        </div>
+                      </div>
+                    </Card.Content>
+                  </Card>
+                </motion.div>
+              );
+            else;
+          })}
+        </AnimatePresence>
+        <div className="w-full max-w-2xs overflow-x-auto sm:max-w-full">
+          <Pagination className="justify-center">
+            <Pagination.Content>
+              {getPageNumbers().map((p, i) =>
+                p === "ellipsis" ? (
+                  <Pagination.Item key={`ellipsis-${i}`}>
+                    <Pagination.Ellipsis />
+                  </Pagination.Item>
+                ) : (
+                  <Pagination.Item key={p}>
+                    <Pagination.Link
+                      isActive={p === page}
+                      onPress={() => setPage(p)}
+                    >
+                      {p}
+                    </Pagination.Link>
+                  </Pagination.Item>
+                ),
+              )}
+            </Pagination.Content>
+          </Pagination>
         </div>
-      </main>
+      </div>
     </div>
   );
 }
