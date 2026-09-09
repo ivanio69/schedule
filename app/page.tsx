3c217f7299ace300eb0b2fc37108e92956f0f641
Ivan Mikhneev <lifecatsdev@gmail.com>
2026-09-09 23:36:44 +0300

    add upcoming rehearsals to dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import DashboardV2 from "@/components/DashboardV2";
+import DashboardRehearsals from "@/components/DashboardRehearsals";
 
 export default function Home() {
-  return <DashboardV2 />;
+  return (
+    <>
+      <DashboardV2 />
+      <DashboardRehearsals />
+    </>
+  );
 }
