b195969194dc6d725637b081affae51bdc140674
Ivan Mikhneev <lifecatsdev@gmail.com>
2026-09-09 22:27:25 +0300

    Show individual lessons in full schedule

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -1,10 +1,12 @@
 import TodaySchedule from "@/components/TodaySchedule";
 import RehearsalPeoplePicker from "@/components/RehearsalPeoplePicker";
+import IndividualScheduleOverlay from "@/components/IndividualScheduleOverlay";
 
 export default function SchedulePage() {
   return (
     <>
       <RehearsalPeoplePicker />
+      <IndividualScheduleOverlay />
       <TodaySchedule />
     </>
   );
