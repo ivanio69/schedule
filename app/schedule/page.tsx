57e0965d32b27acf374416a49d94615eedde4365
Ivan Mikhneev <lifecatsdev@gmail.com>
2026-09-09 22:26:51 +0300

    Remove all rehearsals button

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -1,13 +1,9 @@
 import TodaySchedule from "@/components/TodaySchedule";
 import RehearsalPeoplePicker from "@/components/RehearsalPeoplePicker";
-import AllRehearsalsButton from "@/components/AllRehearsalsButton";
 
 export default function SchedulePage() {
   return (
     <>
-      <div className="rehearsals-toolbar">
-        <AllRehearsalsButton />
-      </div>
       <RehearsalPeoplePicker />
       <TodaySchedule />
     </>
