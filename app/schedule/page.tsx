006f99a01002f429e515941288d46b57720d7f74
Ivan Mikhneev <lifecatsdev@gmail.com>
2026-09-09 22:06:15 +0300

    Keep full schedule view alongside dashboard

---
+++ app/schedule/page.tsx
@@ -0,0 +1,6 @@
+import ScheduleApp from "@/components/ScheduleApp";
+import RehearsalPeoplePicker from "@/components/RehearsalPeoplePicker";
+
+export default function SchedulePage() {
+  return <><RehearsalPeoplePicker /><ScheduleApp /></>;
+}
