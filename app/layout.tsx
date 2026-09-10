170660d82480ead13e466c1f29e502ccdb91428f
Ivan Mikhneev <lifecatsdev@gmail.com>
2026-09-10 14:38:38 +0300

    Add seminar styles to app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,11 @@ import "./schedule-modal.css";
 import "./dashboard-login.css";
 import "./page-transitions.css";
 import "./loading-state.css";
+import "./micro-interactions.css";
+import "./admin/individual-slots/slots.css";
+import "./individual-slots.css";
+import "./seminars/seminars.css";
+import "./admin/seminars/seminars.css";
 import AppNavigation from "@/components/AppNavigation";
 
 const geist = Geist({
