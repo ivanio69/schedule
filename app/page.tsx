929a6ee22355a819b8810aade2a621167ae89705
Ivan Mikhneev <lifecatsdev@gmail.com>
2026-09-10 13:09:32 +0300

    feat: move individual slots off dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import DashboardV2 from "@/components/DashboardV2";
 import DashboardRehearsals from "@/components/DashboardRehearsals";
-import IndividualSlots from "@/components/IndividualSlots";
 
 export default function Home() {
   return (
     <>
       <DashboardV2 />
       <DashboardRehearsals />
-      <IndividualSlots />
     </>
   );
 }
