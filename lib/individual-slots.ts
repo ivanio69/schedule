93e805d5043ea8b4db58922e5682b7bf0ab2a014
Ivan Mikhneev <lifecatsdev@gmail.com>
2026-09-10 13:04:11 +0300

    Add individual lesson slot type

---
+++ lib/individual-slots.ts
@@ -0,0 +1,13 @@
+export type IndividualSlot = {
+  id: string;
+  subject: string;
+  professor: string;
+  auditorium: string;
+  date: string;
+  timeStart: string;
+  timeEnd: string;
+  note: string;
+  studentId: string | null;
+  createdAt: string;
+  updatedAt: string;
+};
