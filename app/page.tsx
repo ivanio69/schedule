674d19edad9d28ab2aa08af4d90e7dd3473962c4
Ivan Mikhneev <lifecatsdev@gmail.com>
2026-09-10 13:59:13 +0300

    Add booked individual slots to dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import DashboardV2 from "@/components/DashboardV2";
 import DashboardRehearsals from "@/components/DashboardRehearsals";
+import BookedIndividualSlotsInDashboard from "@/components/BookedIndividualSlotsInDashboard";
 
 export default function Home() {
   return (
     <>
       <DashboardV2 />
+      <BookedIndividualSlotsInDashboard />
       <DashboardRehearsals />
     </>
   );
