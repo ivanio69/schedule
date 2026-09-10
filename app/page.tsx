5294958f99c3f515b28a0b702552d1ba71c3788c
Ivan Mikhneev <lifecatsdev@gmail.com>
2026-09-10 13:06:38 +0300

    Show bookable individual slots on student dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import DashboardV2 from "@/components/DashboardV2";
 import DashboardRehearsals from "@/components/DashboardRehearsals";
+import IndividualSlots from "@/components/IndividualSlots";
 
 export default function Home() {
   return (
     <>
       <DashboardV2 />
       <DashboardRehearsals />
+      <IndividualSlots />
     </>
   );
 }
