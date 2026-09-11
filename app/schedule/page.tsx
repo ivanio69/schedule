4c3da3af7e0c8bec6b9ec508da1be5d14cf14ff4
Ivan Mikhneev <lifecatsdev@gmail.com>
2026-09-11 13:20:58 +0300

    feat: show scholarship badge in schedule

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -1,12 +1,14 @@
 import TodaySchedule from "@/components/TodaySchedule";
 import RehearsalPeoplePicker from "@/components/RehearsalPeoplePicker";
 import IndividualScheduleOverlay from "@/components/IndividualScheduleOverlay";
+import ScheduleScholarshipBadge from "@/components/ScheduleScholarshipBadge";
 
 export default function SchedulePage() {
   return (
     <>
       <RehearsalPeoplePicker />
       <IndividualScheduleOverlay />
+      <ScheduleScholarshipBadge />
       <TodaySchedule />
     </>
   );
