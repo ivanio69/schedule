ca41d29779990e0a31a083e5652edcd36eaf8601
Ivan Mikhneev <lifecatsdev@gmail.com>
2026-09-11 10:54:11 +0300

    style: apply shared page headings

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ import "./micro-interactions.css";
 import "./admin/individual-slots/slots.css";
 import "./individual-slots.css";
 import "./day-tabs-polish.css";
+import "./page-headings.css";
 import AppNavigation from "@/components/AppNavigation";
 
 const geist = Geist({ variable: "--font-geist", subsets: ["latin", "cyrillic"] });
